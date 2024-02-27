e026823277e69925f3dbd8aa7664b6ea5b02e13f
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-02-27 14:00:49 +0000

    Update launch_substation.py
    data_path chnage

--- demo-PET/fed_substation/launch_substation.py
+++ demo-PET/fed_substation/launch_substation.py
@@ -28,7 +28,7 @@ from federate_helper import FEDERATE_HELPER, CURVES_TO_PLOT
 
 
 """================================Declare something====================================="""
-data_ = './data/exp(test)/'
+data_path = './data/exp(test)/'
 if not os.path.exists(data_path):
     os.makedirs(data_path)
 configfile = 'TE_Challenge_agent_dict.json'
