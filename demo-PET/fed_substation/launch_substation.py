98ba6b8b42cada3946d47195ba218c2254cb58b8
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-02-28 14:49:05 +0000

    Update launch_substation.py

--- demo-PET/fed_substation/launch_substation.py
+++ demo-PET/fed_substation/launch_substation.py
@@ -256,9 +256,9 @@ while (time_granted < StopTime):
    # secondPath = '../demo-PET/fed_gridlabd/'
   #  secondPath = os.chdir('..')
   #  secondPath2 = os.chdir('..')
-    os.chdir('..')
+    os.chdir('../')
     os.getcwd()
-    os.chdir('..')
+    os.chdir('../')
     filePath = './fed_gridlabd/'      
     with open(filePath+'house_TE_ChallengeH_metrics.json', encoding='utf-8') as f:
         prosumer_dict = json.loads(f.read()) # federate_config is the dict data structure
