b02f97cae4bbb32fac9bfb313107e2d81130f1a0
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-02-29 12:22:37 +0000

    Update launch_substation.py

--- demo-PET/fed_substation/launch_substation.py
+++ demo-PET/fed_substation/launch_substation.py
@@ -278,7 +278,7 @@ os.chdir("/PEMT-CoSim-Test/PEMT-CoSim-Test/demo-PET/")
    # os.getcwd()
 trialPath = '../'
 filePath = 'fed_gridlabd/'      
-with open(path_base + 'exp1' +'house_TE_ChallengeH_metrics.json', encoding='utf-8') as f:  #used to be with open(filePath + ...)####
+with open('/data/exp(dyB-1-3kw)/house_TE_ChallengeH_metrics.json', encoding='utf-8') as f:  #used to be with open(filePath + ...)####
     prosumer_dict = json.loads(f.read()) # federate_config is the dict data structure
     f.close() 
 time_hour_auction = data_dict['time_hour_auction']
