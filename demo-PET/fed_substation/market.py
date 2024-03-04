cabdaca86501b13080321173112acd9067aaff04
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-03-04 10:54:53 +0000

    Update market.py

--- demo-PET/fed_substation/market.py
+++ demo-PET/fed_substation/market.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas
 import helics
-from helics import helicsFederate
+from helics import HelicsFederate
 from pandas import DataFrame, Series
 from scipy.stats import iqr
 
