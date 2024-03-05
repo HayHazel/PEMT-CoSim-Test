5d929afd2f484e11dfa08cb69723b7913fb91266
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-03-05 16:54:04 +0000

    Update generate_case.py

--- demo-PET/generate_case.py
+++ demo-PET/generate_case.py
@@ -12,6 +12,8 @@ import numpy as np
 from glmhelper import GLM_HELPER
 import my_tesp_support_api.api as tesp
 from fed_weather.TMY3toCSV import weathercsv
+from scenario import PETScenario
+
 
 """0. generate a glm file (TE_Challenge.glm) according to user's preference"""
 class GLOBAL_Configuration:
