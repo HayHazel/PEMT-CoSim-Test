f61a75349a6f832981a093877e4e185bf5b4e055
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-03-07 12:17:53 +0000

    Update helics_config_helper.py
    changed | to or

--- demo-V2G/helics_config_helper.py
+++ demo-V2G/helics_config_helper.py
@@ -8,14 +8,14 @@ def pub(obj, prop, prop_type, info=False):
         "global": False,
         "key": f"{obj}#{prop}",
         "type": prop_type
-    } | ({"info": {"object": obj, "property": prop}} if info else {})
+    } or ({"info": {"object": obj, "property": prop}} if info else {})
 
 
 def sub(source, obj, prop, prop_type, info=False, target_name=None):
     return {
         "key": f"{source}/{obj}#{prop}",
         "type": prop_type
-    } | ({"info": {"object": obj, "property": target_name or prop}} if info else {})
+    } or ({"info": {"object": obj, "property": target_name or prop}} if info else {})
 
 
 class HelicsConfigHelper:
