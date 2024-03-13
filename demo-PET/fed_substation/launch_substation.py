4754f48342a87843b155592d8387d8aa3ffb9cd7
HayHazel <101203298+HayHazel@users.noreply.github.com>
2024-03-13 11:17:15 +0000

    Update launch_substation.py

--- demo-PET/fed_substation/launch_substation.py
+++ demo-PET/fed_substation/launch_substation.py
@@ -18,7 +18,7 @@ import random
 import psutil
 import subprocess
 import pickle
-from PET_Prosumer2 import HOUSE, VPP        # import user-defined my_hvac class for hvac controllers
+from PET_Prosumer import HOUSE, VPP        # import user-defined my_hvac class for hvac controllers
 from datetime import datetime
 from datetime import timedelta
 from my_auction import AUCTION  # import user-defined my_auction class for market
